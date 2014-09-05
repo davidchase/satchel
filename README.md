c6987b9cd0cf72169c6584d12cbbe0badcc76a01
David Annopolsky <dannopolsky@freepeople.com>
2014-09-05 18:08:11 -0400

    started mods

---
+++ README.md
@@ -0,0 +1,13 @@
+BrowserDB
+=========
+
+A client database that uses IndexedDB, WebSQL, or localStorage depending on browser support.
+
+Notice
+------
+This is a fork of [localForage](https://github.com/mozilla/localForage) by Mozilla,
+however without all of the underlining kerfuffle such as AMD or bower.
+
+It uses [commonjs](http://wiki.commonjs.org/wiki/Modules/1.1.1) pattern for modules and [browserify](http://browserify.org/) to package them for the browser.
+
+If that doesn't suit your needs go with the orginal or just use the compiled version. 
---
+++ src/index.js
@@ -0,0 +1,84 @@
+/* global indexedDB */
+'use strict';
+// Require what we need
+var Promise = Promise || require('es6-promise');
+var indexeddb = require('./drivers/indexeddb');
+var localstorage = require('./drivers/localstorage');
+// might not need it since its no longer
+// supported and 5MB of localstorage is good
+var websql = require('./drivers/websql');
+
+// Declare internals
+var internals = {};
+
+internals.driverType = {
+    INDEXEDDB: 'asyncStorage',
+    LOCALSTORAGE: 'localStorageWrapper',
+    WEBSQL: 'webSQLStorage'
+};
+
+internals.driverSupport = function() {
+    var result = {};
+    result[internals.driverType.WEBSQL] = !!openDatabase;
+    result[internals.driverType.INDEXEDDB] =
+        indexedDB && indexedDB.open('_current', 1).onupgradeneeded === null;
+    result[internals.driverType.LOCALSTORAGE] = !!localStorage;
+
+    return result;
+};
+
+var BrowserDB = function() {
+    this.config = {
+        description: '',
+        name: 'browserDB',
+        // Default DB size is _JUST UNDER_ 5MB, as it's the highest size
+        // we can use without a prompt.
+        size: 4980736,
+        storeName: 'keyvaluepairs',
+        version: 1.0
+    };
+    this.driverSet = null;
+    this.ready = false;
+};
+
+var BrowserDBProto = BrowserDB.prototype;
+
+BrowserDBProto.config = function(options) {
+    var self = this;
+    var types = {
+        'object': function() {
+            if (self.ready) {
+                throw new Error("Can't call config() after browserDB has been used.");
+            }
+            for (var item in options) {
+                if (options.hasOwnProperty(item)) {
+                    self.config[item] = options[item];
+                }
+            }
+
+            return true;
+        },
+        'string': function() {
+            return self.config[options];
+        },
+        'undefined': function() {
+            return self.config;
+        }
+    };
+    if (types[typeof options] === 'function') {
+        types[typeof options].call(null);
+    }
+};
+
+BrowserDBProto.driver = function() {
+    return this.driver || null;
+};
+
+BrowserDBProto.ready = function() {};
+BrowserDBProto.setDriver = function() {};
+BrowserDBProto.supports = function(driverName) {
+    return !!internals.driverSupport[driverName];
+};
+
+// export that goodness !
+module.exports = BrowserDB;
